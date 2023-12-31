9f7ca212cd4f91b45ea3da7a67e3e33cca27cdbd
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-12-31 18:08:39 +0100

    Add form submission functionality to Contact component

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import Contactme from "../Pictures/contactme.png";
 import "./Contact.css"; // Import the CSS file
+import { useState } from "react";
 export default function Contact() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await fetch(event.target.action, {
+      method: 'POST',
+      body: new FormData(event.target),
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(responst => {
+      setIsSubmitted(true);
+      event.target.reset()
+    }).catch(error => {
+      console.log(error)
+    });
+  }
   return (
     <section id="contact" className="contact-section">
-        <h2 className="contact-header">Contact Me</h2>
-        <p className="contact-subheader">
-          Have a question or want to work together?</p>
+      <h2 className="contact-header">Contact Me</h2>
+      <p className="contact-subheader">
+        Have a question or want to work together?</p>
       <div className="contact-container">
-          <div className="image-container">
-            <img src={Contactme} alt="ContactMe" />
-          </div>
+        <div className="image-container">
+          <img src={Contactme} alt="ContactMe" />
+        </div>
         <form
-        action="https://formspree.io/f/xbjnjglk"
-        method="POST">
+          action="https://formspree.io/f/xbjnjglk"
+          method="POST"
+          onSubmit={handleSubmit}>
           <div className="input-field">
             <label htmlFor="name" className="input-label">
               Name
@@ -54,6 +73,7 @@ export default function Contact() {
             Submit
           </button>
         </form>
+        {isSubmitted && <p className="success-message">Thank you for your message. I will get back to you soon!</p>}
       </div>
     </section>
   );
